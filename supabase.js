d95a7388c992db37c98ff8518210a5db0bfdfd3a
LEOAF <didier08020@gmail.com>
2026-03-01 10:27:08 +0100

    Delete supabase.js

--- supabase.js
+++
@@ -1,4 +0,0 @@
-const supabaseUrl = 'https://kpkpptqtuuhnbdntuluc.supabase.co';
-const supabaseKey = 'sb_publishable_aws8RQ085UqKFBUWfojluQ_o9R9WKBb';
-const { createClient } = supabase;
-const client = createClient(supabaseUrl, supabaseKey);
