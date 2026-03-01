cdd1a42b39b74aa108a87d297a423b2159b4802d
LEOAF <didier08020@gmail.com>
2026-03-01 10:19:11 +0100

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,4 @@
+const supabaseUrl = 'https://kpkpptqtuuhnbdntuluc.supabase.co';
+const supabaseKey = 'sb_publishable_aws8RQ085UqKFBUWfojluQ_o9R9WKBb';
+const { createClient } = supabase;
+const client = createClient(supabaseUrl, supabaseKey);
